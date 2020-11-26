44439a7aeae2edd172d33f9f2a0c1e317655da07
Documenter.jl <documenter@juliadocs.github.io>
2020-11-26 20:26:49 +0000

    build based on fe9acfb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FHIRClientJSON","category":"page"},{"location":"#FHIRClientJSON","page":"Home","title":"FHIRClientJSON","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FHIRClientJSON]","category":"page"}]
+}
