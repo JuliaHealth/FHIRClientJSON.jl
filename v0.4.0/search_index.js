ef21db257b5444d347d02af91af51f66e4b44ae6
Documenter.jl <documenter@juliadocs.github.io>
2021-03-16 14:30:55 +0000

    build based on 5d4ab13

---
+++ v0.4.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FHIRClientJSON","category":"page"},{"location":"#FHIRClientJSON.jl-(deprecated-in-favor-of-[FHIRClient.jl](https://github.com/JuliaHealth/FHIRClient.jl))","page":"Home","title":"FHIRClientJSON.jl (deprecated in favor of FHIRClient.jl)","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"FHIRClientJSON.jl is deprecated.","category":"page"},{"location":"","page":"Home","title":"Home","text":"Please use FHIRClient.jl instead.","category":"page"}]
+}
